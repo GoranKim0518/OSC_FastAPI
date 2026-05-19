ed07385ae530fdb0d950e9d4c9850b6b90df681c
GoranKim0518 <sungmin051830@gmail.com>
2026-05-19 16:35:02 +0900

    fix: remove json

--- course.py
+++ course.py
@@ -1,26 +1,50 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 from model import Course
+import json
 
 course_router = APIRouter()
 
-courses_list = []
+JSON_FILE = "courses.json"
+
+def load_courses():
+    try:
+        with open(JSON_FILE, "r", encoding="utf-8") as f:
+            return json.load(f)
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e)
+        )
+
+def save_courses(data):
+    try:
+        with open(JSON_FILE, "w", encoding="utf-8") as f:
+            json.dump(data, f, ensure_ascii=False, indent=2)
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e)
+        )
 
 @course_router.post("/courses")
-async def add_course(course: Course) -> dict:
-    courses_list.append(course)
+async def add_course(course: Course):
+    courses_list = load_courses()
+    courses_list.append(course.model_dump())
+    save_courses(courses_list)
     return {
-        "msg": "course post success"
-    }   #try-catch 추가
+        "msg": "course post success",
+        "course": course
+    }
 
 @course_router.get("/courses")
-async def retrieve_courses() -> dict:
+async def retrieve_courses():
+    courses_list = load_courses()
     return {
         "courses": courses_list
     }
-        #json import로 변경
 
-
-        
 #@course_router.get("/todo/{todo_id}")
 #async def get_single_todo(todo_id: int = Path(..., title = "the ID of the todo to retrieve")) -> dict:
 #    for todo in courses_list:
